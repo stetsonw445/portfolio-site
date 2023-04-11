17d76019583fd085c6ecf7e4f50ac3bef698f379
stetsonw445 <85764643+stetsonw445@users.noreply.github.com>
2023-04-11 15:30:10 -0500

    Adding resources, code changes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const changeColorLink = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.btn');
 
+let isGradient1 = true;
 
 document.addEventListener("DOMContentLoaded", function() {
   const changeColorLink = document.querySelector('.change-color');
@@ -10,18 +14,52 @@ document.addEventListener("DOMContentLoaded", function() {
 
       changeColorLink.addEventListener('click', toggleBackgroundGradient);
 
-      let isGradient1 = true;
 
 function toggleBackgroundGradient() {
   isGradient1 = !isGradient1;
   if (isGradient1) {
     body.classList.add('background-gradient-1');
     body.classList.remove('background-gradient-2');
+    buttons.forEach((button) => {
+      button.classList.remove('background-gradient-2');
+      button.classList.add('background-gradient-1');
+    });
   } else {
     body.classList.add('background-gradient-2');
     body.classList.remove('background-gradient-1');
+    buttons.forEach((button) => {
+      button.classList.remove('background-gradient-1');
+      button.classList.add('background-gradient-2');
+    });
   }
 }
+
+// Get the image element
+const img = document.querySelector('.fade-in');
+
+// Function to check if the element is in view
+function isInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to show the image when it's in view
+function showImage() {
+  if (isInViewport(img)) {
+    img.classList.add('visible');
+    window.removeEventListener('scroll', showImage);
+  }
+}
+
+// Add an event listener to show the image when the page is scrolled
+window.addEventListener('scroll', showImage);
+
+
 });
 
 
